1405693500f5c68974dd3a007b37f1b09f2fb15b
Alec Marcum <amzero081@gmail.com>
2017-09-21 00:07:38 -0700

    Add files via upload
    ;)

---
+++ Matrix/src/matrix/MatrixDriver.java
@@ -0,0 +1,105 @@
+package matrix;
+import java.util.*;
+
+
+public class MatrixDriver {
+
+    public static void main(String[] args) 
+    {
+        Matrix myMat = getInp();
+        Matrix urMat = getInp();
+        boolean checkOp = checkOp(myMat, urMat);
+        
+        //Testing
+        System.out.println(myMat.toString() + "\n");
+        System.out.println(urMat.toString());
+        
+    }
+
+    //Gets user input for matrix
+    public static Matrix getInp()
+    {
+     return new Matrix(getRow(),getCol());
+     
+    }
+    
+     public static int getRow()
+    {
+     Scanner in = new Scanner(System.in);
+     int x;
+
+     System.out.print("Please enter how many rows ");
+     x = in.nextInt();
+     
+     return x;
+     
+    }
+     
+     public static int getCol()
+    {
+     Scanner in = new Scanner(System.in);
+     
+     int y;
+ 
+     System.out.print("Please enter how many columns ");
+     y = in.nextInt();
+     
+     return y;
+    }
+     
+    public static int getOp()
+    {
+        Scanner in = new Scanner(System.in);
+        int o = 0;
+ 
+        System.out.print("Which operation would you like to use? \n 1. Add \n 2. Subtract \n 3. dotProduct \n 4. Hadamard \n");
+        o = in.nextInt();
+        
+        return o;
+    }
+    
+    public static boolean checkOp(Matrix A, Matrix B) //checks Operation with validate
+    {
+        boolean opValid;
+        int op = getOp();
+        if(!validate(A, B, op))
+        {
+            System.out.print("Invalid input \n");
+            return opValid = false;
+        }
+        return opValid = true;
+    }
+    
+    public static boolean validate(Matrix A, Matrix B, int op) //checks dimensions of matrices
+    {
+        boolean valid;
+        if(op==3)
+        {
+            if(A.row == B.col || A.col == B.row)
+            {
+                return valid = true;
+            }
+            
+            else
+            {
+                return valid = false;
+            }
+        }
+        else if(op>0 && op<5)
+        {
+            if(A.row == B.row && A.col == B.col)
+                {
+                    return valid=true;
+                }
+            else
+            {
+                return valid = false;
+            }
+        }
+        else
+        {
+            return valid = false;
+        }
+        
+    }
+}
